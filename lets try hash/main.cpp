6dd5e46085debf36bc56d19adc54e5a7f4803d7a
Abel Atnafu <abeljayirv@gmail.com>
2020-09-13 02:54:06 -0700

    another new size

--- lets try hash/main.cpp
+++ lets try hash/main.cpp
@@ -15,7 +15,7 @@
 using namespace std;
 
 int main(){
-    Hashtable nobHill(4);
+    Hashtable nobHill(10);
     nobHill.insertNewEmployeeBadge(3230, "Abel");
     nobHill.insertNewEmployeeBadge(17, "Jordan");
     nobHill.insertNewEmployeeBadge(100, "Dan");
